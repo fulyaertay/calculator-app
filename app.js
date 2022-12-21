4bbf3c3db28dbff6d0fc977861c81e64bea8ddad
Fulya <fulya.ertay@gmail.com>
2022-12-22 00:21:45 +0300

    feat: add clickButton function

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 let buttons=document.querySelectorAll(".grid button");
 let display=document.querySelector(".display");
 //! butonlara basıldığında çalışacak olan fonksiyon
-buttons=forEach(function(button){
-    button.addEventListener("click",function(e){
-        display.innerText=display.innerText + e.target.innerText;
-    })
+function clickButton(){
+    buttons.forEach(function(button){
+        button.addEventListener("click",function(e){
+            display.innerText=display.innerText + e.target.innerText;
+        })
 
-})
+    })
+}
+
+clickButton();

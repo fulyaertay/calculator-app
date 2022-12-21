8bc1b3da5dbb3a776666af323199dd534e180773
Fulya <fulya.ertay@gmail.com>
2022-12-22 01:01:12 +0300

    fix: clickButton function added

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-let buttons=document.querySelectorAll(".grid button");
-let display=document.querySelector(".display");
+const buttons=document.querySelectorAll(".grid button");
+const display=document.querySelector(".inputVal");
 //! butonlara basıldığında çalışacak olan fonksiyon
 function clickButton(){
     buttons.forEach(function(button){
         button.addEventListener("click",function(e){
-            display.innerText=display.innerText + e.target.innerText;
+            if (e.target.innerText == "=") {
+                try {
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Invalid";
+                }
+            }
+            else if(e.target.innerText == "C"){
+                display.innerText = "";
+            }
+            else if(e.target.innerText == "←"){
+                display.innerText = display.innerText.slice(0,-1);
+            }
+            else{
+                display.innerText += e.target.innerText;
+            }
+            
         })
-
-    })
+    });
 }
 
 clickButton();
